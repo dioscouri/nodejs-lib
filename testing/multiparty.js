e8c7df038113696a4947f41e5ea65d8d90b07160
Bushev Yuriy <bushevuv@gmail.com>
2016-11-04 16:32:44 +0300

    Update dependencies.

---
+++ testing/multiparty.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * Created by Yuriy Bushev <bushevuv@gmail.com> on 04/11/2016.
+ */
+
+'use strict';
+
+
+var multiparty = require('multiparty');
+var http = require('http');
+var util = require('util');
+
+http.createServer(function(req, res) {
+    if (req.url === '/upload' && req.method === 'POST') {
+        // parse a file upload
+        var form = new multiparty.Form();
+
+        form.parse(req, function(err, fields, files) {
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.write('received upload:\n\n');
+            res.end(util.inspect({fields: fields, files: files}));
+        });
+
+        return;
+    }
+
+    // show a file upload form
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+        '<form action="/upload" enctype="multipart/form-data" method="post">'+
+        '<input type="text" name="title"><br>'+
+        '<input type="file" name="upload" multiple="multiple"><br>'+
+        '<input type="submit" value="Upload">'+
+        '</form>'
+    );
+}).listen(8081);
